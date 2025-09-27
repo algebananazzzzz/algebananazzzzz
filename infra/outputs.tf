ffe9f7db5c76a9e69c2bd4f757affa3dee035a78
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-09-28 03:59:23 +0800

    🐞 fix(terraform outputs): hotfix add cloudfront_distribution_id

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
 output "origin_bucket_name" {
   value = module.cloudfront_s3_origin.new_bucket.bucket
 }
+
+output "cloudfront_distribution_id" {
+  value = module.cloudfront_s3_origin.distribution.id
+}
