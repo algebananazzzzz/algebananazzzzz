4c1c49653588534926c61eda2e9e8c516552d513
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2026-09-22 04:46:20 +0800

    chore: remove old Gatsby site

--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "origin_bucket_name" {
-  value = module.cloudfront_s3_origin.new_bucket.bucket
-}
-
-output "cloudfront_distribution_id" {
-  value = module.cloudfront_s3_origin.distribution.id
-}
